0a4967a4306f966a88945cfde30a2de7f4b1938b
Clinton Reimann Jr. <jackfreckles@hotmail.com>
2026-03-09 12:51:20 -0600

    Modified main and tests - Currently not finished, just want to move to different pc

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+'''
+Clinton Reimann
+CSCI 332 Spring 2025
+Programming Assignment #class18 (main)
+
+I acknowledge that I have worked on this assignment independently, except
+where explicitly noted and referenced. Any collaboration or use of external
+resources has been properly cited. I am fully aware of the consequences of
+academic dishonesty and agree to abide by the university's academic integrity
+policy. I understand the importance the consequences of plagiarism.
+'''
+
 from typing import List, Tuple
 
 Point = Tuple[float, float]
 
 def convex_hull_jarvis(points: List[Point]) -> List[Point]:
-   pass
-
+    #angle = (p2[0] - p1[0])*(p3[1] - p2[1]) - (p2[1] - p1[1])*(p3[0] - p2[0])
+    encapsulatingPoints = []
+    for i in range(len(points)):
+        for j in range(len(points)):
+            orientation = None
 
 if __name__ == "__main__":
     points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)] 
--- tests.py
+++ tests.py
@@ -1,3 +1,15 @@
+'''
+Clinton Reimann
+CSCI 332 Spring 2025
+Programming Assignment #class18 (tests)
+
+I acknowledge that I have worked on this assignment independently, except
+where explicitly noted and referenced. Any collaboration or use of external
+resources has been properly cited. I am fully aware of the consequences of
+academic dishonesty and agree to abide by the university's academic integrity
+policy. I understand the importance the consequences of plagiarism.
+'''
+
 import unittest
 from main import convex_hull_jarvis
 
